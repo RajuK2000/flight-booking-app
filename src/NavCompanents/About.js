5f30328a33dc9e6370c9e11d83a9d2d2ebc2c586
RajukumarK2000 <kasarapurajukumar04@gmail.com>
2024-06-06 15:38:15 +0530

    dep

--- src/NavCompanents/About.js
+++ src/NavCompanents/About.js
@@ -1,6 +1,6 @@
 import NavBar from "../Components/NavBar/navBar";
 import Footer from "../Components/Footer/footer"
-import Home from "../Components/Home/home";
+// import Home from "../Components/Home/home";
 
 
 const About=()=>{
