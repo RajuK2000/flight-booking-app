2e1e94b06a4102df771440543884c9f93a201717
RajukumarK2000 <kasarapurajukumar04@gmail.com>
2024-06-17 12:19:43 +0530

    travelars

--- src/Components/Travelers/travelers.js
+++ src/Components/Travelers/travelers.js
@@ -12,36 +12,30 @@ import "aos/dist/aos.css"
 import { useEffect } from "react"
 
 
-const travelers=[
-    {
-        id:1,
-        destinationImage: paris,
-        travelerImage:person ,
-        travelerName:"Alian" ,
-        socialLink: "@Alian@yy201"
-    },
-    {
-        id:2,
-        destinationImage: IndGate,
-        travelerImage:person2 ,
-        travelerName:"Rajiv" ,
-        socialLink: "@Rajiv12"
-    },
-    {
-        id:3,
-        destinationImage: japan,
-        travelerImage:japanPerson,
-        travelerName:"Khufua" ,
-        socialLink: "@Kuku5210"
-    },
-    {
-        id:4,
-        destinationImage: dubai,
-        travelerImage:dubaiPerson,
-        travelerName:"AliKhan" ,
-        socialLink: "@Alik%420"
-    }
-]
+// const travelers=[
+   
+//     {
+//         id:2,
+//         destinationImage: IndGate,
+//         travelerImage:person2 ,
+//         travelerName:"Rajiv" ,
+//         socialLink: "@Rajiv12"
+//     },
+//     {
+//         id:3,
+//         destinationImage: japan,
+//         travelerImage:japanPerson,
+//         travelerName:"Khufua" ,
+//         socialLink: "@Kuku5210"
+//     },
+//     {
+//         id:4,
+//         destinationImage: dubai,
+//         travelerImage:dubaiPerson,
+//         travelerName:"AliKhan" ,
+//         socialLink: "@Alik%420"
+//     }
+// ]
 const Travelars=()=>{
     useEffect(()=>{
         Aos.init({duration:2000})
@@ -53,28 +47,64 @@ const Travelars=()=>{
                 <h2 data-aos="fade-down" data-aos-duration="2500" >Top travelers of this month!</h2>
 
                 <div className="travelersContainer">
-                 {
-                    travelers.map(({id,destinationImage,travelerImage,travelerName,socialLink})=>{
-                        return(
+                 {/* { */}
+                    {/* travelers.map(({id,destinationImage,travelerImage,travelerName,socialLink})=>{ */}
+                        {/* return( */}
 
-                            <div data-aos="fade-up" data-aos-duration="2500" key={id} className="singleTraveler">
-                            <img  src={destinationImage} className="destinationImage" alt=""/>
+                            <div data-aos="fade-up" data-aos-duration="2500"  className="singleTraveler1">
+                            <img  src={paris} className="destinationImage" alt="mk"/>
                             <div className="travelerDetails">
                                 <div className="travelerPictucres">
-                                    <img src={travelerImage} className="travelerImage" alt=""/>
+                                    <img src={person} className="travelerImage" alt="mj"/>
                                 </div>
                                 <div className="travelerName">
-                                    <span>{travelerName}</span>
-                                   <p> {socialLink}</p>
+                                    <span>Alian</span>
+                                   <p> @Alian@yy201</p>
                                 </div>
                             </div>
                         </div>
-                        )
 
-                    })
-                 }
-                  
-                    
+                        <div data-aos="fade-down" data-aos-duration="2500"  className="singleTraveler2">
+                            <img  src={IndGate} className="destinationImage" alt="mk"/>
+                            <div className="travelerDetails">
+                                <div className="travelerPictucres">
+                                    <img src={person2} className="travelerImage" alt="mj"/>
+                                </div>
+                                <div className="travelerName">
+                                    <span>Rajiv</span>
+                                   <p> @Rajiv12</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div data-aos="fade-up" data-aos-duration="2500"  className="singleTraveler3">
+                            <img  src={japan} className="destinationImage" alt="mk"/>
+                            <div className="travelerDetails">
+                                <div className="travelerPictucres">
+                                    <img src={japanPerson} className="travelerImage" alt="mj"/>
+                                </div>
+                                <div className="travelerName">
+                                    <span>Khufua</span>
+                                   <p> @Kuku5210</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div data-aos="fade-down" data-aos-duration="2500"  className="singleTraveler4">
+                            <img  src={dubai} className="destinationImage" alt="mk"/>
+                            <div className="travelerDetails">
+                                <div className="travelerPictucres">
+                                    <img src={dubaiPerson} className="travelerImage" alt="mj"/>
+                                </div>
+                                <div className="travelerName">
+                                    <span>AliKhan</span>
+                                   <p>@Alik%420</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* // )
+                    // })
+                 }       */}
                 </div>
             </div>
         </div>
